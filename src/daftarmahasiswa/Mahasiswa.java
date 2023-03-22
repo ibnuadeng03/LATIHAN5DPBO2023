81f7e080106fd9ac076a483ef3693bf791650a9d
ibnuadeng03 <100753882+ibnuadeng03@users.noreply.github.com>
2023-03-22 16:15:49 +0700

    Unggah File Latihan 5 bag src

---
+++ src/daftarmahasiswa/Mahasiswa.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package daftarmahasiswa;
+
+/**
+ *
+ * @author sekar
+ */
+public class Mahasiswa {
+    private String nim;
+    private String nama;
+    private String nilai;
+    private String gender;
+    
+    public Mahasiswa(String nim, String nama, String nilai, String gender){
+        this.nim = nim;
+        this.nama = nama;
+        this.nilai = nilai;
+        this.gender=gender;
+    }
+    
+    public void setNim(String nim) {
+        this.nim = nim;
+    }
+    
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+    
+    public void setNilai(String nilai) {
+        this.nilai = nilai;
+    }
+    
+    public void setGender(String gender) {
+        this.gender = gender;
+    } 
+
+    public String getNim() {
+        return this.nim;
+    }
+    
+    public String getNama() {
+        return this.nama;
+    }
+    
+    public String getNilai() {
+        return this.nilai;
+    }
+    
+    public String getGender() {
+        return this.gender;
+    }
+}
+
+/*  
+	Saya Ibnu Adeng Kurnia NIM 2101769 mengerjakan latihan ke-5 
+	dalam mata kuliah desain dan pemrograman berorientasi objek C2 2023
+	untuk keberkahanNya maka saya tidak melakukan kecurangan seperti yang telah dispesifikasikan. 
+	Aamiin.
+*/
+
+/*  Design ini disusun dan/atau digunakan hanya untuk lingkungan sendiri.
+	Tidak untuk menjadi konsumsi/kepentingan umum.
+	Hanya untuk melengkapi tugas DPBO 2023.
+*/
